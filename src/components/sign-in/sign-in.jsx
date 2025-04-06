e9bfdcd0381bd11f1139feee27ebecc104631818
oanaMitricoiu <oana.mitri01@gmail.com>
2025-04-06 20:51:00 +0300

    create sign up component,the form component and the button component

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,4 +1,5 @@
 import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase';
+import SignUpForm from '../sign-up-form/sign-up-form';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
@@ -6,10 +7,12 @@ const SignIn = () => {
         const userDocRef = createUserDocumentFromAuth(user);
         console.log(userDocRef);
     }
+
     return (
         <div>
             <h1>Sign in page</h1>
             <button onClick={logGoogleUser}>Sign in with Google</button>
+            <SignUpForm/>
         </div>
     )
 }
